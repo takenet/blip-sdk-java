cdfae8821afdd95f71ffff82624d5fbae13f5b36
André Bires <andrebires@gmail.com>
2017-09-09 11:32:44 -0300

    Using gradle instead of maven

--- src/src/main/java/net/take/blip/Sender.java
+++ src/src/main/java/net/take/blip/Sender.java
@@ -4,35 +4,30 @@ import org.limeprotocol.Command;
 import org.limeprotocol.Document;
 import org.limeprotocol.Message;
 import org.limeprotocol.Node;
+import org.limeprotocol.messaging.contents.PlainText;
 import org.limeprotocol.network.CommandChannel;
 import org.limeprotocol.network.MessageChannel;
 import org.limeprotocol.network.NotificationChannel;
-import org.limeprotocol.messaging.contents;
 
 import java.io.IOException;
 import java.util.UUID;
 
 public interface Sender extends MessageChannel, NotificationChannel, CommandChannel {
-
     Command processCommand(Command requestCommand);
 
-    default void sendMessage(final String content, final Node to) {
-
-
-        PlainText plainTextContent = new PlainText();
-
-
+    default void sendMessage(final String content, final Node to) throws IOException {
+        sendMessage(new PlainText(content), to);
     }
 
     default void sendMessage(final Document content, final Node to) throws IOException {
-        if (content == null) throw new IllegalArgumentException("Content cannot be null");
+        if (content == null) throw new IllegalArgumentException("Content is required");
 
-        Message message = new Message(UUID.randomUUID().toString()) {{
+        Message message = new Message() {{
+            setId(UUID.randomUUID().toString());
             setTo(to);
             setContent(content);
         }};
 
         sendMessage(message);
     }
-
 }
